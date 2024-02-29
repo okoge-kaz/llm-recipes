db23a01dfcf0bf5aa34fb59fd029ee0a98ef825d
kazuki <kaz.tokyo.tech20@gmail.com>
2024-02-29 14:21:36 +0900

    feat: swallow-instruct-v1.0

--- tools/model-upload/upload.sh
+++ tools/model-upload/upload.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-start=20000
-end=20000
+start=2430
+end=2430
 increment=5000
 
-upload_base_dir=/bb/llm/gaf51275/llama/converted-hf-checkpoint/mistral-7B-VE/the-vault-lr_2e-5-minlr_6.6e-7
+upload_base_dir=/bb/llm/gaf51275/llama/converted-hf-checkpoint/Swallow-7b-VE-chat/baseline-lr_2e-5-minlr_2e-6/
 
 for ((i = start; i <= end; i += increment)); do
   upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
 
   python tools/model-upload/upload.py \
     --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Mistral-7B-VE-the-vault-iter$(printf "%07d" $i)
+    --repo-name tokyotech-llm/Swallow-7b-VE-instruct-v1.0-baseline-lr_2e-5-minlr_2e-6-iter$(printf "%07d" $i)
 done
