4f95a28ca9620503372e8c3ebf848db5631e2156
kazuki <kaz.tokyo.tech20@gmail.com>
2024-02-29 11:41:52 +0900

    feat: update iteration upload script

--- tools/model-upload/upload.sh
+++ tools/model-upload/upload.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-start=672
-end=672
+start=20000
+end=20000
 increment=5000
 
-upload_base_dir=/model/fujii/hf_checkpoints/swallow-7b/imitative-open-asst2
+upload_base_dir=/bb/llm/gaf51275/llama/converted-hf-checkpoint/mistral-7B-VE/the-vault-lr_2e-5-minlr_6.6e-7
 
 for ((i = start; i <= end; i += increment)); do
   upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
 
   python tools/model-upload/upload.py \
     --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Swallow-7b-VE-instruct-imitative-open-asst2-2epoch-iter$(printf "%07d" $i)
+    --repo-name tokyotech-llm/Mistral-7B-VE-the-vault-iter$(printf "%07d" $i)
 done
