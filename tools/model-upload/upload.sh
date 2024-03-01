fecaa56657f7a2d9e01575603d28f22c6a5fc9be
kazuki <kaz.tokyo.tech20@gmail.com>
2024-03-01 20:12:45 +0900

    feat: NEFTune

--- tools/model-upload/upload.sh
+++ tools/model-upload/upload.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-start=606
-end=606
+start=1284
+end=1284
 increment=5000
 
-upload_base_dir=/bb/llm/gaf51275/llama/converted-hf-checkpoint/Swallow-7b-VE-chat/baseline-lr_2e-5-minlr_2e-6-GB256
+upload_base_dir=/bb/llm/gaf51275/llama/converted-hf-checkpoint/Swallow-7b-VE-chat/imitation-2-oasst2-top1-lr_2e-5-minlr_2e-6-GB_64
 
 for ((i = start; i <= end; i += increment)); do
   upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
 
   python tools/model-upload/upload.py \
     --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Swallow-7b-VE-instruct-v1.0-baseline-GB256-lr_2e-5-minlr_2e-6-iter$(printf "%07d" $i)
+    --repo-name tokyotech-llm/Swallow-7b-VE-instruct-v1.0-imitation-2-oasst2-top1-lr_2e-5-minlr_2e-6-GB_64-iter$(printf "%07d" $i)
 done
