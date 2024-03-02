8b6b6677430b9ca8ce8664eea1ddbd7bd1a9474d
kazuki <kaz.tokyo.tech20@gmail.com>
2024-03-02 13:04:12 +0900

    chore: update ckpt path

--- tools/model-upload/upload.sh
+++ tools/model-upload/upload.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-start=1284
-end=1284
+start=2080
+end=2080
 increment=5000
 
-upload_base_dir=/bb/llm/gaf51275/llama/converted-hf-checkpoint/Swallow-7b-VE-chat/imitation-2-oasst2-top1-lr_2e-5-minlr_2e-6-GB_64
+upload_base_dir=/bb/llm/gaf51275/llama/converted-hf-checkpoint/Swallow-7b-VE-chat/imitation-1-and-2-lr_2e-5-minlr_2e-6-GB_64
 
 for ((i = start; i <= end; i += increment)); do
   upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
 
   python tools/model-upload/upload.py \
     --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Swallow-7b-VE-instruct-v1.0-imitation-2-oasst2-top1-lr_2e-5-minlr_2e-6-GB_64-iter$(printf "%07d" $i)
+    --repo-name tokyotech-llm/Swallow-7b-VE-instruct-v1.0-imitation-1-and-2-lr_2e-5-minlr_2e-6-GB_64-iter$(printf "%07d" $i)
 done
