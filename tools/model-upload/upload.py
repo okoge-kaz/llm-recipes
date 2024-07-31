3c0d06b4199888328952186299938d5ae3938808
kazuki <kaz.tokyo.tech20@gmail.com>
2024-07-31 19:55:20 +0900

    feat: upload script

--- tools/model-upload/upload.py
+++ tools/model-upload/upload.py
@@ -1,25 +1,41 @@
 import os
 import argparse
-
+from tqdm import tqdm
 from huggingface_hub import HfApi, create_repo
 
 
+def upload_directory(api, local_dir, repo_name, repo_type, branch_name):
+    for root, _, files in os.walk(local_dir):
+        for file in tqdm(files, desc=f"Uploading files in {root}"):
+            local_path = os.path.join(root, file)
+            repo_path = os.path.relpath(local_path, local_dir)
+
+            print(f"Uploading {repo_path} to branch {branch_name}...")
+            api.upload_file(
+                path_or_fileobj=local_path,
+                path_in_repo=repo_path,
+                repo_id=repo_name,
+                repo_type=repo_type,
+                commit_message=f"Upload {repo_path}",
+                revision=branch_name,
+            )
+            print(f"Successfully uploaded {repo_path}!")
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--ckpt-path", type=str)
-parser.add_argument("--repo-name", type=str)
-parser.add_argument("--branch-name", type=str, default="main")
+parser.add_argument("--ckpt-path", type=str, help="Path to the checkpoint directory")
+parser.add_argument("--repo-name", type=str, help="Name of the Hugging Face repository")
+parser.add_argument("--branch-name", type=str, default="main", help="Branch name in the repository")
 args = parser.parse_args()
 
 converted_ckpt: str = args.ckpt_path
 repo_name: str = args.repo_name
 branch_name: str = args.branch_name
+
 try:
     create_repo(repo_name, repo_type="model", private=True)
 except Exception as e:
-    print(f"repo {repo_name} already exists! error: {e}")
-    pass
-
-files = os.listdir(converted_ckpt)
+    print(f"Repository {repo_name} already exists! Error: {e}")
 
 api = HfApi()
 if branch_name != "main":
@@ -29,17 +45,9 @@ if branch_name != "main":
             repo_type="model",
             branch=branch_name,
         )
-    except Exception:
-        print(f"branch {branch_name} already exists, try again...")
-print(f"to upload: {files}")
-for file in files:
-    print(f"Uploading {file} to branch {branch_name}...")
-    api.upload_file(
-        path_or_fileobj=os.path.join(converted_ckpt, file),
-        path_in_repo=file,
-        repo_id=repo_name,
-        repo_type="model",
-        commit_message=f"Upload {file}",
-        revision=branch_name,
-    )
-    print(f"Successfully uploaded {file} !")
+    except Exception as e:
+        print(f"Branch {branch_name} already exists. Error: {e}")
+
+print(f"Starting upload of directory: {converted_ckpt}")
+upload_directory(api, converted_ckpt, repo_name, "model", branch_name)
+print("Upload completed successfully!")
