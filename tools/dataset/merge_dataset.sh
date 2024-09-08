80347a88e4799983915daf2a0ba41096ef8b5e4d
kazuki <kaz.tokyo.tech20@gmail.com>
2024-09-09 01:14:17 +0900

    feat: dataset merge

--- tools/dataset/merge_dataset.sh
+++ tools/dataset/merge_dataset.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-INPUT_DIR=/bb/llm/gaf51275/datasets/raw/instruct/general/oasst2-33k-ja
-OUTPUT_DIR=/bb/llm/gaf51275/datasets/raw/instruct/training/exp1-4
+set -e
+
+INCLUDE_REDACTED=true
+FILTERD_SCORE=7
+NEXT_TOKEN_PERCENT=0.25
+
+OUTPUT_DIR=/bb/llm/gaf51275/datasets/raw/instruct/training/exp2-filtered-$FILTERD_SCORE-next_token-$NEXT_TOKEN_PERCENT
+
+if $INCLUDE_REDACTED; then
+  OUTPUT_DIR=$OUTPUT_DIR-redacted
+fi
 
 mkdir -p $OUTPUT_DIR
 
@@ -23,10 +32,41 @@ done
 python tools/dataset/fileter.py \
   --input_file $MERGED_FILE \
   --output_file $OUTPUT_DIR/train.jsonl \
-  --threshold 7
+  --threshold $FILTERD_SCORE
 
 rm $MERGED_FILE
 
-echo "Done"
-
+echo "Filtered open assistant data:"
 wc -l $OUTPUT_DIR/train.jsonl
+
+if $INCLUDE_REDACTED; then
+  LMSYS_FILE=/bb/llm/gaf51275/datasets/raw/instruct/lmsys-chat-1m/sft/lmsys-chat-1m-train.jsonl
+else
+  LMSYS_FILE=/bb/llm/gaf51275/datasets/raw/instruct/lmsys-chat-1m/sft/lmsys-chat-1m-train-no-redacted.jsonl
+fi
+
+cat $LMSYS_FILE >> $OUTPUT_DIR/train.jsonl
+
+INSTRUCTION_SAMPLES=$(wc -l $OUTPUT_DIR/train.jsonl | awk '{print $1}')
+NEXT_TOKEN_SAMPLES=$(echo "$INSTRUCTION_SAMPLES * $NEXT_TOKEN_PERCENT / 1" | bc)
+
+python tools/dataset/extract_jsonl.py \
+  --input-path /bb/llm/gaf51275/datasets/raw/instruct/next-token/next-token-prediction_500k/format/merged.jsonl \
+  --output-path $OUTPUT_DIR/next-token.jsonl \
+  --num-lines $NEXT_TOKEN_SAMPLES \
+  --seed 1234
+
+echo "Next token data:"
+wc -l $OUTPUT_DIR/next-token.jsonl
+
+cat $OUTPUT_DIR/next-token.jsonl >> $OUTPUT_DIR/train.jsonl
+
+echo "Total data:"
+wc -l $OUTPUT_DIR/train.jsonl
+
+rm $OUTPUT_DIR/next-token.jsonl
+
+# indexing
+
+python tools/pre-process/index_dataset.py \
+  --data-file-path $OUTPUT_DIR/train.jsonl
