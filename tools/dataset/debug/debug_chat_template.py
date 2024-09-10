8080e0742fe40feed4e96b4f7ea6ad49cc6eeb62
kazuki <kaz.tokyo.tech20@gmail.com>
2024-09-10 23:22:17 +0900

    chore: tools script (move directory)

--- tools/dataset/debug/debug_chat_template.py
+++ tools/dataset/debug/debug_chat_template.py
@@ -45,7 +45,7 @@ conversations_with_output = [
     }
 ]
 
-chat_template: str = "{% set loop_messages = messages %}{% for message in loop_messages %}{% set content = '<|start_header_id|>' + message['role'] + '<|end_header_id|>\n\n'+ message['content'] | trim + '<|eot_id|>' %}{% if loop.index0 == 0 %}{% set content = bos_token + content %}{% endif %}{{ content }}{% endfor %}{% if add_generation_prompt %}{{ '<|start_header_id|>assistant<|end_header_id|>\n\n' }}{% endif %}"
+chat_template: str = "{% set loop_messages = messages %}{% for message in loop_messages %}{% set content = '<|start_header_id|>' + message['role'] + '<|end_header_id|>\n\n'+ message['content'] | trim + '<|eot_id|>' %}{% if loop.index0 == 0 %}{% set content = bos_token + content %}{% endif %}{{ content }}{% endfor %}{% if add_generation_prompt %}{{ '<|start_header_id|>assistant<|end_header_id|>\n\n' }}{% endif %}"  # noqa:
 
 print("before apply chat template")
 
