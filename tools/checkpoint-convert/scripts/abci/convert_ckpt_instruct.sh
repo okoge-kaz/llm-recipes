1f536275d045c542a3d764657e4d05ccaa3ba87e
kazuki <kaz.tokyo.tech20@gmail.com>
2024-09-12 21:17:53 +0900

    feat: checkpoint convert & inference script

--- tools/checkpoint-convert/scripts/abci/convert_ckpt_instruct.sh
+++ tools/checkpoint-convert/scripts/abci/convert_ckpt_instruct.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -l rt_F=1
+#$ -l rt_AF=1
 #$ -l h_rt=1:00:00
 #$ -j y
 #$ -o outputs/convert/ckpt/
@@ -21,7 +21,7 @@ export HF_HOME="/groups/gag51395/.cache/huggigface"
 # swich virtual env
 source .env/bin/activate
 
-CHECKPOINT_DIR=/bb/llm/gaf51275/2024/checkpoints/Llama-3.1-8B-Instruct/exp1-1/LR_1e-5_MINLR_1e-6_WD_0.1_GC_1
+CHECKPOINT_DIR=/bb/llm/gaf51275/2024/checkpoints/Llama-3.1-8B-Instruct/exp2-1/LR_2.5e-5_MINLR_2.5e-6_WD_0.1_GC_1
 LATEST_ITERATION=$(cat ${CHECKPOINT_DIR}/latest_iteration.txt)
 
 echo "LATEST_ITERATION=${LATEST_ITERATION}"
--- tools/inference/inference_abci.sh
+++ tools/inference/inference_abci.sh
@@ -20,7 +20,7 @@ set -e
 # swich virtual env
 source .env/bin/activate
 
-INFERENCE_MODEL_DIR=/bb/llm/gaf51275/2024/checkpoints/pytorch-to-hf/Llama-3.1-8B-Instruct/exp2-1/LR_2.5e-5_MINLR_2.5e-6_WD_0.1_GC_1/iter_0005078
+INFERENCE_MODEL_DIR=/bb/llm/gaf51275/2024/checkpoints/pytorch-to-hf/Llama-3.1-8B-Instruct/exp2-4/LR_1e-6_MINLR_1e-7_WD_0.1_GC_1/iter_0004000
 
 python tools/inference/inference.py \
   --model-path $INFERENCE_MODEL_DIR \
@@ -32,4 +32,22 @@ python tools/inference/inference.py \
   --model-path $INFERENCE_MODEL_DIR \
   --tokenizer-path $INFERENCE_MODEL_DIR \
   --prompt "会社法について説明してください。" \
+  --chat-template
+
+python tools/inference/inference.py \
+  --model-path $INFERENCE_MODEL_DIR \
+  --tokenizer-path $INFERENCE_MODEL_DIR \
+  --prompt "東京工業大学のキャンパスはどこにありますか？" \
+  --chat-template
+
+python tools/inference/inference.py \
+  --model-path $INFERENCE_MODEL_DIR \
+  --tokenizer-path $INFERENCE_MODEL_DIR \
+  --prompt "1+4+8の答えはいくつでしょうか？" \
+  --chat-template
+
+python tools/inference/inference.py \
+  --model-path $INFERENCE_MODEL_DIR \
+  --tokenizer-path $INFERENCE_MODEL_DIR \
+  --prompt "Pythonでデータ構造のUnionFindクラスを作成してください。" \
   --chat-template
