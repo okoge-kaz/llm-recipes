bd3859e809241107400a1429e92e77e62205fbf4
kazuki <kaz.tokyo.tech20@gmail.com>
2024-10-04 13:44:44 +0900

    chore: update dataset merge & convert script

--- tools/dataset/merge_dataset_abci.sh
+++ tools/dataset/merge_dataset_abci.sh
@@ -3,111 +3,54 @@
 set -e
 
 # Control variables
-INCLUDE_REDACTED=false
-FILTERD_SCORE=0
-NEXT_TOKEN_PERCENT=0
-USE_OPEN_ASSISTANT=false
-USE_ONLY_ENGLISH_OPEN_ASSISTANT=false
-USE_ENGLISH_LMSYS=true
-USE_MAGPIE_ULTRA=true
-USE_GEMMA_MAGPIE=true
-CUSTOM_OUTPUT_DIR=""
+USE_ENGLISH_LMSYS=false
+USE_SYNTH_EN_WO_TEMPLATE=false
+USE_SYNTH_JA_WO_TEMPLATE=false
+USE_MAGPIE_ULTRA_EN=false
+USE_MAGPIE_ULTRA_JA=false
+USE_GEMMA_MAGPIE=false
 
 # Base output directory
 BASE_OUTPUT_DIR="/bb/llm/gaf51275/datasets/raw/instruct/training"
-
-if [ -z "$CUSTOM_OUTPUT_DIR" ]; then
-  OUTPUT_DIR="$BASE_OUTPUT_DIR/exp2-filtered-$FILTERD_SCORE-next_token-$NEXT_TOKEN_PERCENT"
-else
-  OUTPUT_DIR="$CUSTOM_OUTPUT_DIR"
-fi
-
-if $INCLUDE_REDACTED; then
-  OUTPUT_DIR="${OUTPUT_DIR}-redacted"
-fi
-
-if ! $USE_OPEN_ASSISTANT; then
-  OUTPUT_DIR="${OUTPUT_DIR}-no-oasst"
-elif $USE_ONLY_ENGLISH_OPEN_ASSISTANT; then
-  OUTPUT_DIR="${OUTPUT_DIR}-en-oasst"
-fi
-
-if $USE_ENGLISH_LMSYS; then
-  OUTPUT_DIR="${OUTPUT_DIR}-en-lmsys"
-fi
-
-if $USE_MAGPIE_ULTRA; then
-  OUTPUT_DIR="${OUTPUT_DIR}-magpie-ultra"
-fi
-
-if $USE_GEMMA_MAGPIE; then
-  OUTPUT_DIR="${OUTPUT_DIR}-gemma-magpie"
-fi
+OUTPUT_DIR="${BASE_OUTPUT_DIR}/exp2-12-3"
 
 mkdir -p $OUTPUT_DIR
-
-if $USE_OPEN_ASSISTANT; then
-  if $USE_ONLY_ENGLISH_OPEN_ASSISTANT; then
-    FILES=(
-      "/bb/llm/gaf51275/datasets/raw/instruct/general/oasst2-top1-en-chat-sft/data/train.jsonl"
-    )
-  else
-    FILES=(
-      "/bb/llm/gaf51275/datasets/raw/instruct/general/oasst2-33k-ja/lm_filtered_split_1.jsonl"
-      "/bb/llm/gaf51275/datasets/raw/instruct/general/oasst2-33k-ja/lm_filtered_split_2.jsonl"
-      "/bb/llm/gaf51275/datasets/raw/instruct/general/oasst2-33k-ja/lm_filtered_split_3.jsonl"
-      "/bb/llm/gaf51275/datasets/raw/instruct/general/oasst2-33k-ja/lm_filtered_split_4.jsonl"
-      "/bb/llm/gaf51275/datasets/raw/instruct/general/oasst2-top1-en-chat-sft/data/train.jsonl"
-    )
-  fi
-
-  MERGED_FILE=$OUTPUT_DIR/merged_oasst.jsonl
-
-  for FILE in "${FILES[@]}"; do
-    cat $FILE >> $MERGED_FILE
-  done
-
-  # filter
-  python tools/dataset/fileter.py \
-    --input_file $MERGED_FILE \
-    --output_file $OUTPUT_DIR/train.jsonl \
-    --threshold $FILTERD_SCORE
-
-  rm $MERGED_FILE
-
-  echo "Filtered open assistant data:"
-  wc -l $OUTPUT_DIR/train.jsonl
-else
-  # Open Assistant データを使用しない場合は空のファイルを作成
-  touch $OUTPUT_DIR/train.jsonl
-  echo "Skipped Open Assistant data processing."
+if [ -f $OUTPUT_DIR/train.jsonl ]; then
+  rm $OUTPUT_DIR/train.jsonl
 fi
 
-# 日本語のLMSYSデータを常に使用
-if $INCLUDE_REDACTED; then
-  JA_LMSYS_FILE=/bb/llm/gaf51275/datasets/raw/instruct/lmsys-chat-1m/sft/lmsys-chat-1m-train.jsonl
+# Japanese LMSYS
+if $USE_SYNTH_JA_WO_TEMPLATE; then
+  JA_LMSYS_FILE=/bb/llm/gaf51275/datasets/raw/instruct/lmsys-chat-1m/sft/lmsys-chat-1m-synth-ja-wo-pii-and-template-instructions-train.jsonl
 else
   JA_LMSYS_FILE=/bb/llm/gaf51275/datasets/raw/instruct/lmsys-chat-1m/sft/lmsys-chat-1m-train-ja-no-redacted.jsonl
 fi
 
 cat $JA_LMSYS_FILE >> $OUTPUT_DIR/train.jsonl
 
-# 英語のLMSYSデータを追加でオプションとして使用
+# English LMSYS
 if $USE_ENGLISH_LMSYS; then
-  if $INCLUDE_REDACTED; then
-    EN_LMSYS_FILE=/bb/llm/gaf51275/datasets/raw/instruct/lmsys-chat-1m/sft/lmsys-chat-1m-train-en.jsonl
+  if $USE_SYNTH_EN_WO_TEMPLATE; then
+    EN_LMSYS_FILE=/bb/llm/gaf51275/datasets/raw/instruct/lmsys-chat-1m/sft/lmsys-chat-1m-synth-en-wo-pii-and-template-instructions-train.jsonl
   else
     EN_LMSYS_FILE=/bb/llm/gaf51275/datasets/raw/instruct/lmsys-chat-1m/sft/lmsys-chat-1m-train-en-no-redacted.jsonl
   fi
+
   cat $EN_LMSYS_FILE >> $OUTPUT_DIR/train.jsonl
   echo "Added English LMSYS data"
 fi
 
 # Add magpie-ultra dataset processing
-if $USE_MAGPIE_ULTRA; then
-  MAGPIE_ULTRA_FILE=/bb/llm/gaf51275/datasets/raw/instruct/synthetic/magpie-ultra-v0.1/data/train.jsonl
+if $USE_MAGPIE_ULTRA_EN; then
+  MAGPIE_ULTRA_FILE=/bb/llm/gaf51275/datasets/raw/instruct/synthetic/magpie-ultra-v0.1/data/train_en.jsonl
   cat $MAGPIE_ULTRA_FILE >> $OUTPUT_DIR/train.jsonl
-  echo "Added magpie-ultra data"
+  echo "Added magpie-ultra en data"
+fi
+
+if $USE_MAGPIE_ULTRA_JA; then
+  MAGPIE_ULTRA_FILE=/bb/llm/gaf51275/datasets/raw/instruct/synthetic/magpie-ultra-v0.1/data/train_ja.jsonl
+  cat $MAGPIE_ULTRA_FILE >> $OUTPUT_DIR/train.jsonl
+  echo "Added magpie-ultra ja data"
 fi
 
 # Add gemma-magpie dataset processing
@@ -117,24 +60,8 @@ if $USE_GEMMA_MAGPIE; then
   echo "Added gemma-magpie data"
 fi
 
-INSTRUCTION_SAMPLES=$(wc -l $OUTPUT_DIR/train.jsonl | awk '{print $1}')
-NEXT_TOKEN_SAMPLES=$(echo "($INSTRUCTION_SAMPLES / (1 - $NEXT_TOKEN_PERCENT)) * $NEXT_TOKEN_PERCENT / 1" | bc)
-
-python tools/dataset/extract_jsonl.py \
-  --input-path /bb/llm/gaf51275/datasets/raw/instruct/next-token/next-token-prediction_500k/format/merged.jsonl \
-  --output-path $OUTPUT_DIR/next-token.jsonl \
-  --num-lines $NEXT_TOKEN_SAMPLES \
-  --seed 1234
-
-echo "Next token data:"
-wc -l $OUTPUT_DIR/next-token.jsonl
-
-cat $OUTPUT_DIR/next-token.jsonl >> $OUTPUT_DIR/train.jsonl
-
 echo "Total data:"
 wc -l $OUTPUT_DIR/train.jsonl
-
-rm $OUTPUT_DIR/next-token.jsonl
 
 # indexing
 
