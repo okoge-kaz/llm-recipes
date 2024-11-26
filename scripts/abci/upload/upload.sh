b5a004952924c421ec5010782090cc7d3f82ab97
kazuki <kaz.tokyo.tech20@gmail.com>
2024-11-26 21:05:43 +0900

    chore: delete unnecessary job scripts

--- scripts/abci/upload/upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-python llama-recipes/scripts/kotoba/upload/upload.py \
-  --ckpt-path "" \
-  --repo-name ""
--- tools/inference/inference.sh
+++ tools/inference/inference.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$ -cwd
-#$ -l node_q=1
+#$ -l node_f=1
 #$ -l h_rt=0:1:00:00
 #$ -o outputs/inference/$JOB_ID.log
 #$ -e outputs/inference/$JOB_ID.log
@@ -9,18 +9,20 @@
 # module load
 module use /gs/fs/tga-NII-LLM/modules/modulefiles
 
-module load ylab/cuda/12.1
-module load ylab/cudnn/8.9.7
-module load ylab/nccl/cuda-12.2/2.20.5
+module load ylab/cuda/12.4
+module load ylab/cudnn/9.1.0
+module load ylab/nccl/cuda-12.4/2.21.5
 module load ylab/hpcx/2.17.1
 module load ninja/1.11.1
+
+source .env/bin/activate
 
 set -e
 
 # swich virtual env
 source .env/bin/activate
 
-INFERENCE_MODEL_DIR=/gs/bs/tga-NII-LLM/checkpoints/fsdp-hf/Llama-3-8B-Instruct-v0.2/LR_1e-5_MINLR_1e-6_WD_0.1_GC_1-dist-ckpt/iter_0000078
+INFERENCE_MODEL_DIR=/gs/bs/tga-NII-LLM/checkpoints/megatron-to-hf/Llama-3.1-8b-v0.9/tp2-pp1-ct1/iter_0000100
 
 python tools/inference/inference.py \
   --model-path $INFERENCE_MODEL_DIR \
