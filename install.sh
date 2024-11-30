ad47e885ea1407487661715205a659085ca60467
kazuki <kaz.tokyo.tech20@gmail.com>
2024-11-30 17:30:37 +0900

    env: torch 2.5.1

--- install.sh
+++ install.sh
@@ -1,41 +1,25 @@
-#!/bin/bash
-#$ -l rt_AF=1
-#$ -l h_rt=4:00:00
-#$ -j y
-#$ -o outputs/
+#!/bin/sh
 #$ -cwd
+#$ -l node_q=1
+#$ -l h_rt=1:00:00
+#$ -p -5
 
-set -e
+# Load modules
+module use /gs/fs/tga-NII-LLM/modules/modulefiles
 
-# module load
-source /etc/profile.d/modules.sh
-module use /groups/gag51395/modules/modulefiles
+module load ylab/cuda/12.4
+module load ylab/cudnn/9.1.0
+module load ylab/nccl/cuda-12.4/2.21.5
+module load ylab/hpcx/2.17.1
+module load ninja/1.11.1
 
-module load cuda/12.1/12.1.1
-module load cudnn/cuda-12.1/9.0.0
-module load nccl/2.17/2.17.1-1
-module load hpcx/2.12
-module load gcc/11.4.0
-
-# swich virtual env
-source .env/bin/activate
-
-# pip version up
 pip install --upgrade pip
+pip install --upgrade wheel cmake ninja packaging
 
-# install nightly pytorch
-# ref: https://github.com/pytorch/torchtitan/blob/eef8bb2b1b6f0875ab0581079e1511d51654910e/torchtitan/parallelisms/utils.py#L11-L29
-pip install --pre torch==2.5.0.dev20240912+cu121 --index-url https://download.pytorch.org/whl/nightly/cu121
+# install nvidia pytorch
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu124
 
-# pip install requirements
+# Install packages
 pip install -r requirements.txt
 
-# distirbuted training requirements
-pip install mpi4py
-
-# huggingface requirements
-pip install huggingface_hub
-
-# install flash-atten
-pip install ninja packaging wheel
 pip install flash-attn --no-build-isolation
--- scripts/tsubame/install.sh
+++ scripts/tsubame/install.sh
@@ -7,28 +7,19 @@
 # Load modules
 module use /gs/fs/tga-NII-LLM/modules/modulefiles
 
-module load ylab/cuda/12.1
+module load ylab/cuda/12.4
 module load ylab/cudnn/9.1.0
-module load ylab/nccl/cuda-12.4/2.20.5
-module load ylab/hpcx/2.18.1
+module load ylab/nccl/cuda-12.4/2.21.5
+module load ylab/hpcx/2.17.1
 module load ninja/1.11.1
 
-# Set environment variables
-source .env/bin/activate
-
-# pip version up
 pip install --upgrade pip
 pip install --upgrade wheel cmake ninja packaging
 
-# pip install requirements
+# install nvidia pytorch
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu124
+
+# Install packages
 pip install -r requirements.txt
 
-# distirbuted training requirements
-pip install mpi4py
-
-# huggingface requirements
-pip install huggingface_hub
-
-# install flash-atten
-pip install ninja packaging wheel
 pip install flash-attn --no-build-isolation
